c23fcdee4ed4f043d017028c3ce52e3066b4902f
Neeraj Reddy <neerajreddy0059@gmail.com>
2024-03-19 16:16:08 +0530

    Added Few Initial Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello, let the dev begin</h1>
-      </header>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+
+
+import './Header.css'
+
+function Header() {
+  return(
+    <p>In the Header Tab</p>
+  )
+}
+
+export default Header;
